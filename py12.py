85ae272eb5500ac4da343851e04833066db02895
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-24 22:55:37 +0530

    Update py12.py

--- py12.py
+++ py12.py
@@ -1,29 +1,29 @@
 #LIST
 # list = used to store mulitiple items in a single varialbe!
 
-#food = ["idli","dose","poori","vade","pakoda"]
+food = ["idli","dose","poori","vade","pakoda"]
 
-#food[3] = "bajji"
-#print(food)
-#some useful functions for list
-#food.append("bajji")
-#food.remove("idli")
-#food.pop()
-#food.insert(0,"parota")
-#food.sort()
-#food.clear()
+food[3] = "bajji"
+print(food)
+some useful functions for list
+food.append("bajji")
+food.remove("idli")
+food.pop()
+food.insert(0,"parota")
+food.sort()
+food.clear()
 
-#for x in food:
-#    print(x,end=" ")
-#print(food[-2])
+for x in food:
+    print(x,end=" ")
+print(food[-2])
 
 #2D lists = list of lists!
 #         = a list which has colletion of lists!
 #         = lists within one list
 
-#tindi = ["idli","capathi","dose","palav"]
-#snacks = ["kara","chips","chakkli","batani"]
-#drinks = ["orange","pineapple","apple","lemon"]
+tindi = ["idli","capathi","dose","palav"]
+snacks = ["kara","chips","chakkli","batani"]
+drinks = ["orange","pineapple","apple","lemon"]
 
-#menu = [tindi,snacks,drinks]
-#print(menu[0][1])
+menu = [tindi,snacks,drinks]
+print(menu[0][1])
